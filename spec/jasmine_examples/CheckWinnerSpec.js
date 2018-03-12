ab6a6fe40ab8308ff38f605b4c9fed724127c73d
Jonathon Nordquist <jonathon.nordquist@gmail.com>
2018-03-12 00:31:16 -0500

    Implement basic minimax function

--- spec/jasmine_examples/CheckWinnerSpec.js
+++ spec/jasmine_examples/CheckWinnerSpec.js
@@ -20,34 +20,34 @@ describe('checkWinningBoard function', function() {
   });
 
   it('should return true for a row', function() {
-    expect(checkWinner.checkWinningBoard(['x', 'x', 'x', '-', '-', '-', '-', '-', '-'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard(['-', '-', '-', '-', '-', '-', 'x', 'x', 'x'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard(['x', 'x', 'x', 'x', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard(['-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', '-', 'x', 'x', 'x', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', 'x', 'x', '3', '4', '5', '6', '7', '8'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['0', '1', '2', '3', '4', '5', 'x', 'x', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', 'x', 'x', 'x', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', 'x', 'x', 'x', 'x'], 'x')).toBe(true);
   });
 
   it('should return true for a column', function() {
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', 'x', '-', '-', 'x', '-', '-'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['-', '-', 'x', '-', '-', 'x', '-', '-', 'x'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', '-', 'x', '-', '-', '-', 'x', '-', '-', '-', 'x', '-', '-', '-'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['-', '-', '-', 'x', '-', '-', '-', 'x', '-', '-', '-', 'x', '-', '-', '-', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', 'x', '4', '5', 'x', '7', '8'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['0', '1', 'x', '3', '4', 'x', '6', '7', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', '3', 'x', '5', '6', '7', 'x', '9', '10', '11', 'x', '13', '14', '15'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['-', '1', '2', 'x', '4', '5', '6', 'x', '8', '9', '10', 'x', '12', '13', '14', 'x'], 'x')).toBe(true);
   });
 
   it('should return true for a diagonal', function() {
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', '-', 'x', '-', '-', '-', 'x'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['-', '-', 'x', '-', 'x', '-', 'x', '-', '-'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', '-', '-', 'x', '-', '-', '-', '-', 'x', '-', '-', '-', '-', 'x'], 'x')).toBe(true);
-    expect(checkWinner.checkWinningBoard( ['-', '-', '-', 'x', '-', '-', 'x', '-', '-', 'x', '-', '-', 'x', '-', '-', '-'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', '-', 'x', '5', '6', '7', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['0', '1', 'x', '3', 'x', '5', 'x', '7', '8'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', '3', '4', 'x', '6', '7', '8', '9', 'x', '11', '12', '13', '14', 'x'], 'x')).toBe(true);
+    expect(checkWinner.checkWinningBoard( ['0', '1', '2', 'x', '4', '5', 'x', '7', '8', 'x', '10', '11', 'x', '13', '14', '15'], 'x')).toBe(true);
   });
 
   it('should return false when no row, column or diagonal', function() {
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', '-', '-', '-', 'x', '-', '-'], 'x')).toBe(false);
-    expect(checkWinner.checkWinningBoard( ['-', '-', '-', '-', '-', '-', '-', '-', '-'], 'x')).toBe(false);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', '3', '4', '5', 'x', '7', '8'], 'x')).toBe(false);
+    expect(checkWinner.checkWinningBoard( ['0', '1', '2', '3', '4', '5', '6', '7', '8'], 'x')).toBe(false);
   });
 
   it('should return false when winning pattern is present for different target value', function() {
-    expect(checkWinner.checkWinningBoard( ['x', 'x', 'x', '-', '-', '-', '-', '-', '-'], 'y')).toBe(false);
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', 'x', '-', '-', 'x', '-', '-'], 'y')).toBe(false);
-    expect(checkWinner.checkWinningBoard( ['x', '-', '-', '-', 'x', '-', '-', '-', 'x'], 'y')).toBe(false);
+    expect(checkWinner.checkWinningBoard( ['x', 'x', 'x', '3', '4', '5', '6', '7', '8'], 'y')).toBe(false);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', 'x', '4', '5', 'x', '7', '8'], 'y')).toBe(false);
+    expect(checkWinner.checkWinningBoard( ['x', '1', '2', '3', 'x', '5', '6', '7', 'x'], 'y')).toBe(false);
   })
 })
--- spec/jasmine_examples/ManageBoardSpec.js
+++ spec/jasmine_examples/ManageBoardSpec.js
@@ -2,18 +2,18 @@ let manageBoard = require('../../lib/ManageBoard.js')
 
 describe('getScore function', function() {
   it('should return "10" from a board where the player has a winning layout', () => {
-    expect(manageBoard.getScore(['x', 'x', 'x', '-', '-', '-', '-', '-', '-'], 'x', 'y')).toEqual(10);
+    expect(manageBoard.getScore(['x', 'x', 'x', '3', '4', '5', '6', '7', '8'], 'x', 'y')).toEqual(10);
   });
   it('should return "-10" from a board where the opponent has a winning layout', () => {
-    expect(manageBoard.getScore(['y', 'y', 'y', '-', '-', '-', '-', '-', '-'], 'x', 'y')).toEqual(-10);
+    expect(manageBoard.getScore(['y', 'y', 'y', '3', '4', '5', '6', '7', '8'], 'x', 'y')).toEqual(-10);
   });
   it('should return "0" from a board where neither player has a winning layout', () => {
-    expect(manageBoard.getScore(['x', 'x', '-', '-', '-', '-', '-', '-', '-'], 'x','y')).toEqual(0);
+    expect(manageBoard.getScore(['x', 'x', '2', '3', '4', '5', '6', '7', '8'], 'x','y')).toEqual(0);
   });
 });
 
 describe('getFreeIndexes function', function() {
   it('should return array of indexes of free spaces from a given board', () => {
-    expect(manageBoard.getFreeIndexes(['y', 'x', 'y', 'y', '-', '-', 'x', 'x', '-'], ['x', 'y'])).toEqual([4, 5, 8]);
+    expect(manageBoard.getFreeIndexes(['y', 'x', 'y', 'y', '4', '5', 'x', 'x', '8'], ['x', 'y'])).toEqual([4, 5, 8]);
   });
 })
